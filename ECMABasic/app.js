dfb3192d7a0c46507b9aa92b8a73662a9c5e5117
SaifAliAnsari786 <saifaliansari477@gmail.com>
2022-06-08 14:55:57 +0545

    Classes

--- ECMABasic/app.js
+++ ECMABasic/app.js
@@ -1,14 +1,14 @@
 //  Normal for loop with var
-for(var i=0;i<10;i++){
-    console.log(i);
-}
+// for(var i=0;i<10;i++){
+//     console.log(i);
+// }
 
 // Funtion loop with var
-for(var i=0; i<10; i++) {
-    setTimeout(()=>{
-        console.log(i);
-    },1000);
-}
+// for(var i=0; i<10; i++) {
+//     setTimeout(()=>{
+//         console.log(i);
+//     },1000);
+// }
 
 // Funtion loop with let
 for(let i=0; i<10; i++) {
